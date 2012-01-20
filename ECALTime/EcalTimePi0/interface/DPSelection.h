4cf170208fa0875f229c3f39be78fd96e492c2a3
sckao <>
2012-01-20 22:49:58 +0000

    add control sample selection

--- ECALTime/EcalTimePi0/interface/DPSelection.h
+++ ECALTime/EcalTimePi0/interface/DPSelection.h
@@ -26,23 +26,18 @@
 #include "TLorentzVector.h"
 
 #include "ECALTime/EcalTimePi0/interface/AnaInput.h"
+
 //#include "MathFunctions.h"
 
 #define MAXSC 50
 #define MAXC 200
 #define MAXXTALINC 25 
-// defined variables to follow are by default set up in ECALTime/EcalTimePi0/interface/EcalTimePhyTreeContent.h
-#ifndef MAXOBJ
 #define MAXOBJ 10
-#endif
-#ifndef MAXPHO
 #define MAXPHO 40
-#endif
-#ifndef MAXVTX
 #define MAXVTX 40
-#endif
 
 typedef pair<int, TLorentzVector> objID ;
+
 
 //class DPSelection : public TObject {
 class DPSelection {
@@ -51,7 +46,8 @@ public:
 
    DPSelection( string datacardfile = "DataCard.txt");     
    ~DPSelection();     
-   
+   //friend class Sync ;   
+
    void Init( TTree* tr ) ;
 
    bool HLTFilter();
@@ -62,6 +58,10 @@ public:
    bool MuonFilter();
 
    bool GammaJetsBackground() ; 
+
+   bool SignalSelection( bool isTightPhoton = true ) ; 
+   bool GammaJetsControlSample( bool isTightPhoton = true ) ; 
+   bool QCDControlSample() ; 
 
    void ResetCuts( string cutName, vector<int>& cutId, vector<double>& newValue ) ;
    void ResetCuts( string cutName, int cutId, double newValue ) ;
@@ -86,19 +86,28 @@ private:
    vector<double> electronCuts ;
    vector<double> muonCuts ;
 
-   
+   unsigned int eventId ;
    float vtxX[MAXVTX],    vtxY[MAXVTX],  vtxZ[MAXVTX],   vtxChi2[MAXVTX], vtxNdof[MAXVTX];
    float jetPx[MAXOBJ],   jetPy[MAXOBJ], jetPz[MAXOBJ],  jetE[MAXOBJ] ;
    float jetNDau[MAXOBJ], jetCM[MAXOBJ], jetCEF[MAXOBJ], jetNHF[MAXOBJ], jetNEF[MAXOBJ];
    float phoPx[MAXPHO],      phoPy[MAXPHO],      phoPz[MAXPHO],     phoE[MAXPHO], phoTime[MAXPHO] ;
    float phoEcalIso[MAXPHO], phoHcalIso[MAXPHO], phoTrkIso[MAXPHO], phoHovE[MAXPHO], phoSmin[MAXPHO], phoSmaj[MAXPHO] ;
    float elePx[MAXOBJ], elePy[MAXOBJ], elePz[MAXOBJ], eleE[MAXOBJ] ;
-   float eleEcalIso[MAXOBJ], eleHcalIso[MAXOBJ], eleTrkIso[MAXOBJ], eleNLostHits[MAXOBJ] ;
+   float eleEcalIso[MAXOBJ], eleHcalIso[MAXOBJ], eleTrkIso[MAXOBJ] ;
+   int   eleNLostHits[MAXOBJ] ;
    float muPx[MAXOBJ], muPy[MAXOBJ], muPz[MAXOBJ], muE[MAXOBJ] ;
    float muEcalIso[MAXOBJ], muHcalIso[MAXOBJ], muTrkIso[MAXOBJ] ;
    float metPx, metPy, metE ;
-   int   nJets, nPhotons, nElectrons, nVertices, nMuons, eventId, trgCut ;
-
+   int   nJets, nPhotons, nElectrons, nVertices, nMuons, trgCut, nSuperClusters, nClusters ;
+   /*
+   float superClusterPhi[MAXSC], superClusterEta[MAXSC] ;
+   float CPIdx[MAXC], clusterTime[MAXC], clusterEnergy[MAXC];
+   float xtalInBCEnergy[MAXC][MAXXTALINC], xtalInBCTime[MAXC][MAXXTALINC], xtalInBCTimeErr[MAXC][MAXXTALINC];
+   float xtalInBCEta[MAXC][MAXXTALINC],    xtalInBCPhi[MAXC][MAXXTALINC],  xtalADC[MAXC][MAXXTALINC] ;
+   float xtalChi2[MAXC][MAXXTALINC], xtalOutTimeChi2[MAXC][MAXXTALINC];
+   float clusterPhi[MAXC], clusterEta[MAXC] ;
+   int   nXtalInBC[MAXC], clusterXtals[MAXC] ;
+   */
    //ClassDef(DPSelection, 1);
 };
 
